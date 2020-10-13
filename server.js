dc668af5d19c1c1ec279f1a185c9d9463c070fce
Andrew Lee <andrewleets@hotmail.com>
2020-10-12 20:09:55 -0700

    added helmet and updated server routers

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const helmet = require('helmet');
+
+app.use(helmet());
+
 mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('connected to database'));
+db.once('open', () => console.log('Connected to database'));
 
 app.use(express.json());
 
-const subscribersRouter = require('./routes/subscribers');
-app.use('/subscribers', subscribersRouter);
+const recordsRouter = require('./routes/records');
+app.use('/records', recordsRouter);
+
+const userRouter = require('./routes/user');
+app.use('/user', userRouter)
 
 app.listen(3000, () => console.log('listening on port 3000'));
