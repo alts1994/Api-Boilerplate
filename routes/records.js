bf8649b67a3a49b86682c26b65c2f2fd743406b5
Andrew Lee <andrewleets@hotmail.com>
2020-10-13 23:54:40 -0700

    formating

--- routes/records.js
+++ routes/records.js
@@ -8,17 +8,17 @@ async function getRecord(req, res, next) {
   try {
     record = await Record.findById(req.params.id);
     if (record == null) {
-      return res.status(404).json({ message: 'Cannot find record' });
+      return res.status(404).json({ message: 'Cannot find record - recordID does not exist' });
     }
   } catch (err) {
-    return res.status(500).json({ message: err.message });
+    return res.status(500).json({ message: `${err.message} || recordID may be invalid` });
   }
 
   res.record = record;
   next();
 }
 
-// Get all records
+// list all records
 router.get('/', async (req, res) => {
   try {
     const records = await Record.find();
